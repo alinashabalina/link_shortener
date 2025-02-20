a7bf7ff65b974f4b95e8e76cbc23e390c972e0d3
Alina Shabalina <alinavshabalina@gmail.com>
2025-02-20 23:44:09 +0100

    feat: add modified link and expiration date to the /link/create method

--- db_manager.py
+++ db_manager.py
@@ -2,10 +2,9 @@ from db import DataBase
 
 
 class Link:
-    def __init__(self, modified, timestamp, duration):
+    def __init__(self, modified, expiration):
         self.modified = modified
-        self.timestamp = timestamp
-        self.duration = duration
+        self.expiration = expiration
 
 
 class LinkDB(DataBase):
@@ -26,11 +25,11 @@ class LinkDB(DataBase):
 ''')
         self.conn.commit()
 
-    def add_link(self, link, modified_link, timestamp):
-        self.cur.execute('INSERT INTO links (initial, modified, timestamp)'
-                         'VALUES (%s, %s, %s)',
-                         (link, modified_link, timestamp))
+    def add_link(self, link, modified_link, timestamp, expiration_time):
+        self.cur.execute('INSERT INTO links (initial, modified, timestamp, expiration_time)'
+                         'VALUES (%s, %s, %s, %s)',
+                         (link, modified_link, timestamp, expiration_time))
         self.conn.commit()
-        self.cur.execute(f"SELECT * FROM links WHERE links.initial = '{link}'")
+        self.cur.execute(f"SELECT * FROM links WHERE links.initial = '{link}' ORDER BY links.id ASC")
         data = self.cur.fetchone()
-        return Link(data[2], data[3], "72 hours")
+        return Link(data[2], data[4])
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import logging
-from datetime import datetime, timezone
+import random
+import string
+from datetime import datetime, timezone, timedelta
 
 from flask import Flask, request, jsonify
 
@@ -13,21 +15,26 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
 
+def _unique_generator(size=10, chars=string.ascii_uppercase + string.digits) -> str:
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
 @app.route("/", methods=["GET"])
 def hello_world():
     response = {"hello": "world"}
     return jsonify(response), 200
 
 
-@app.route("/link", methods=['POST'])
+@app.route("/link/create", methods=['POST'])
 def create_shortened_link():
     try:
         data = json.loads(request.data)
-        modified = "modified"  # to be done
+        modified = "https://opop.com/" + _unique_generator()
         dt = datetime.now(timezone.utc)
-        modified = LinkDB().add_link(data["link"], modified, dt)
-        return jsonify({"your link": modified.modified,
-                        "duration": modified.duration}), 201
+        expiration = datetime.now(timezone.utc) + timedelta(days=3)
+        modified = LinkDB().add_link(data["link"], modified, dt, expiration)
+        return jsonify({"your_link": modified.modified,
+                        "duration_till": modified.expiration}), 201
     except Exception as e:
         logger.error(e)
         return jsonify({"error": "The link is incorrect. Please try again later"}), 400
